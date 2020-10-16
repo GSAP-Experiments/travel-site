acebcc6a3cd5a32db930336784bf887c0c5ed2f1
Harpreet Singh <designsbyharp@gmail.com>
2020-10-16 10:44:52 -0400

    mobile menu click animation 80% complete

--- src/assets/scripts/navigation.js
+++ src/assets/scripts/navigation.js
@@ -10,20 +10,59 @@
 
 import gsap from 'gsap';
 
-const mobileMenuEl = document.querySelector(".js-mobile-menu"),
-      hamburgerEl = gsap.utils.toArray('.js-hamburger');
+const mobileMenuEl  = document.querySelector(".js-mobile-menu"),
+      hamburgerEl   = gsap.utils.toArray(".js-hamburger"),
+      closeEl       = gsap.utils.toArray(".js-close"),
+      closeLeftEl   = document.querySelector(".close--left"),
+      closeRightEl  = document.querySelector(".close--right"),
+      clickTl       = gsap.timeline({paused: true});
+
+      let open = false;
 
 function clicked(){
-   gsap.to(hamburgerEl, {
-      duration: 0.4,
-      scale: 0,
-      ease: 'power2.easeOut',
-      stagger: 0.2
-   })
+   clickTl
+      .to(hamburgerEl, {
+         duration: 0.4,
+         scale: 0,
+         ease: 'power2.easeOut',
+         stagger: 0.2
+      })
+      .add(setDegrees(closeEl))
+      .to(closeEl, {
+         duration: 0.4,
+         scale: 1,
+         ease: 'power2.easeIn',
+         stagger: 0.2   
+      })
+}
+
+function playClicked(){
+   if(!open){
+      clickTl.play();
+      open = true;
+   }else if(open){
+      clickTl.reverse();
+      open = false;
+   }
+   
+}
+
+function setDegrees(elements){
+
+  elements.forEach((element, i) => {
+  
+    if (i === 0) {
+       gsap.set(element, {rotation: 45})
+    } else if (i === 1) {
+      gsap.set(element, { rotation: -45 });
+    }
+
+  });
 }
 
 export default function menu(){
-   mobileMenuEl.addEventListener('click', clicked);
+   clicked();
+   mobileMenuEl.addEventListener('click', playClicked);
 }
 
 
